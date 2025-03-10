17fd63a151b5917ad48f2bbd4b76722777a06e8d
Dm-Rb <dm.rb.418http@gmail.com>
2025-03-10 23:22:32 +0300

    a1.1 - первый рабочий билд

--- keyboards.py
+++ keyboards.py
@@ -2,13 +2,13 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from messages_text import inline_buttons
 
 
-def get_kb__downloadMP3(fuc_type: str, url: str, file_name: str, user_id: int) -> InlineKeyboardMarkup:
+def get_kb__downloadMP3(fuc_type: str, url: str, user_id: int) -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(
         inline_keyboard=[
             [
                 InlineKeyboardButton(
                     text=inline_buttons['download'],
-                    callback_data=f"{fuc_type}:{url}:{file_name}:{str(user_id)}"
+                    callback_data=f"{fuc_type};{url};{str(user_id)}"
                 )
             ]
         ]
