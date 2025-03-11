38ecf0df6ac73a5ca6dd49817c6365c3f3d57b4c
Dm-Rb <dm.rb.418http@gmail.com>
2025-03-11 23:39:07 +0300

    a1.2 - первый рабочий билд

--- callbacks.py
+++ callbacks.py
@@ -1,5 +1,5 @@
 from aiogram import Router, Bot
-from aiogram.types import CallbackQuery, InputFile
+from aiogram.types import CallbackQuery
 from dowload_from_youtube import ytd_obj
 from aiogram.types import FSInputFile
 from pathlib import Path
@@ -19,27 +19,26 @@ async def process_word_response(callback: CallbackQuery, bot: Bot):
     await callback.answer(messages['wait'], show_alert=True)
 
     if url.startswith("https://www.youtu"):
-        # try:
-            # Обработка, логика
-        response = await ytd_obj.download_video_extract_audio(url)
-        # Если статус ответа False - сворачиваем движ
-        if not response['status']:
-            await bot.send_message(chat_id=callback.message.chat.id, text=response['massage'])
-            return
+        try:
+            # Ждём ответ от обработчика
+            response = await ytd_obj.download_video_extract_audio(url)
+            # Если статус ответа False - сворачиваем движ
+            if not response['status']:
+                await bot.send_message(chat_id=callback.message.chat.id, text=response['massage'])
+                return
 
-        path2file = response['massage']
-        print(path2file)
-        # Создаем объект FSInputFile
-        input_file = FSInputFile(path=path2file, filename=Path(path2file).name)
+            path2file = response['massage']
+            print(path2file)
+            # Создаем объект FSInputFile
+            input_file = FSInputFile(path=path2file, filename=Path(path2file).name)
 
-        # Отправляем аудио
-        await bot.send_audio(chat_id=callback.message.chat.id, audio=input_file)
-        if os.path.exists(path2file):
-            # Удаляем файл
-            os.remove(path2file)
-            # Уведомляем пользователя об успешной отправке
-            # await bot.send_message(chat_id=callback.message.chat.id, text="Аудио успешно отправлено!")
+            # Отправляем аудио
+            await bot.send_audio(chat_id=callback.message.chat.id, audio=input_file)
+            if os.path.exists(path2file):
+                # Удаляем файл
+                os.remove(path2file)
 
-        # except Exception as e:
-        #     # Если произошла ошибка, отправляем сообщение об ошибке
-        #     await bot.send_message(chat_id=callback.message.chat.id, text=f"{exceptions['audio_sent']}: {e}")
+
+        except Exception as e:
+            # Если произошла ошибка, отправляем сообщение об ошибке
+            await bot.send_message(chat_id=callback.message.chat.id, text=f"{exceptions['audio_sent']}: {e}")
